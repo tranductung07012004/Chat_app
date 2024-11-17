5ffa12947ff9f14a11f39991f7c518b9f0a87b7c
tung <tranductung07012004@gmail.com>
2024-11-17 21:58:11 +0700

    Adding Nimbus look and feel for the GUI

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,9 +4,25 @@ import GUI.*;
 
 public class Main {
     public static void main(String[] args) {
-        new MainFrameGUI();
+        // Cài đặt Nimbus Look and Feel
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        // Khởi tạo và hiển thị MainFrameGUI
+        java.awt.EventQueue.invokeLater(() -> new MainFrameGUI().setVisible(true));
     }
 }
+
+
+
 
 
 
